53c6f95f10521e3f393327db50c0ed318de88185
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-02-21 18:43:39 +0545

    changed the animation based on key pressed.

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ var screen_size
 func _ready():
 	print("ready")
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -24,3 +25,12 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	#choosing animations
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false 
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0: 
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0 
